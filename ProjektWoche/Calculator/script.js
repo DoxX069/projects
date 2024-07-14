84a0d4f867dc4727eaddab1e6374ddf6d34b9675
DoxX069 <ammonnils@gmail.com>
2024-07-14 17:12:06 +0200

    new commit

---
+++ ProjektWoche/Calculator/script.js
@@ -0,0 +1,52 @@
+var firstNum = '';
+var secondNum = '';
+var op = '';
+var ergebnis;
+
+function zahl(z) {
+	if (op == '') {
+		let display = document.getElementById("dieID");
+		firstNum += z;
+		display.value = firstNum;
+	}
+	else {
+		let display = document.getElementById("dieID");
+		secondNum += z;
+		display.value = secondNum;
+	}
+	let sound = new Audio ("mouseClick.mp3");
+	sound.play();
+}
+function operator(o) {
+	let display = document.getElementById("dieID");
+	display.value = o;
+	op = o;
+	let sound = new Audio ("mouseClick.mp3");
+	sound.play();
+}
+function equal() {
+	let display = document.getElementById("dieID");
+	if (op == '/') {
+		ergebnis = parseInt(firstNum) / parseInt(secondNum);
+	}
+	else if (op == '*') {
+		ergebnis = parseInt(firstNum) * parseInt(secondNum);
+	}
+	else if (op == '-') {
+		ergebnis = parseInt(firstNum) - parseInt(secondNum);
+	}
+	else if (op == '+') {
+		ergebnis = parseInt(firstNum) + parseInt(secondNum);
+	}
+	let sound = new Audio ("anita-max-wynn.mp3");
+	sound.play();
+	remove();
+	display.value = ergebnis;
+}
+function remove() {
+	let display = document.getElementById("dieID");
+	firstNum = '';
+	secondNum = '';
+	op = '';
+	display.value = '';
+}
---
+++ ProjektWoche/Taschenrechner/scripts.js
@@ -0,0 +1,10 @@
+funtction changenum() {
+	let display = document.getElementById("display");
+
+	let a = 1;
+	let b = 2;
+
+	let result = a + b;
+
+	display.innerHTML = result;
+}
