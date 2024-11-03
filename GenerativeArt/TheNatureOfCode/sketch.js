a3e9b380b0b9cc44bc9b74e551bed5988948b350
DoxX069 <ammonnils@gmail.com>
2024-11-03 12:27:44 +0100

    commit

--- GenerativeArt/TheNatureOfCode/sketch.js
+++ GenerativeArt/TheNatureOfCode/sketch.js
@@ -18,16 +18,34 @@ class Walker {
   }
   show() {
     noStroke();
-    fill(255, 100, 100, 100);
+    fill(255, 100, 100, 255);
     circle(this.x, this.y, 2);
   }
   step() {
     let num = random(1);
-    if (num < 0.4) {
-      this.x += 1;
-    } else if (num < 0.6) {
-      this.x -= 2;
+    if (num < 0.6) {
+      if (mouseX < this.x) {
+        this.x -= 1;
+      } 
+      if (mouseX > this.x) {
+        this.x += 1;
+      }
+      if (mouseY < this.y) {
+        this.y -= 1;
+      }
+      if (mouseY > this.y) {
+        this.y += 1;
+      }
+      else {
+        this.x += 0;
+        this.y += 0;
+      }
+    } 
+    else if (num < 0.7) {
+      this.x += 2;
     } else if (num < 0.8) {
+      this.x -= 2;
+    } else if (num < 0.9) {
       this.y += 2;
     } else {
       this.y -= 2;
