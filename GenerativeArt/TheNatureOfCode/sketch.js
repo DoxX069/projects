7974576c7cfff87dccf233dca330b3c95735ff84
DoxX069 <ammonnils@gmail.com>
2024-11-03 11:27:30 +0100

    commit

--- GenerativeArt/TheNatureOfCode/sketch.js
+++ GenerativeArt/TheNatureOfCode/sketch.js
@@ -1,7 +1,36 @@
+let walker;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  walker = new Walker();
+  background(0);
 }
 
 function draw() {
-  background(220);
+  walker.step();
+  walker.show();
+}
+
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+  show() {
+    noStroke();
+    fill(255, 100, 100, 100);
+    circle(this.x, this.y, 2);
+  }
+  step() {
+    let num = random(1);
+    if (num < 0.4) {
+      this.x += 1;
+    } else if (num < 0.6) {
+      this.x -= 2;
+    } else if (num < 0.8) {
+      this.y += 2;
+    } else {
+      this.y -= 2;
+    }
+  }
 }
