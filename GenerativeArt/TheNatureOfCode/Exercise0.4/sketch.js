948be594b37221e9a0cc59aa85bb9a331d771f97
DoxX069 <ammonnils@gmail.com>
2024-11-03 13:07:11 +0100

    exercise0.4

--- GenerativeArt/TheNatureOfCode/Exercise0.4/sketch.js
+++ GenerativeArt/TheNatureOfCode/Exercise0.4/sketch.js
@@ -4,14 +4,14 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 
-  slider = createSlider(0, 1, 0.5, 0.1);
+  slider = createSlider(0, 1, 0, 0.1);
   slider.position(10, 10);
   slider.size(200, 20);
 }
 
 function draw() {
-  let x = randomGaussian(windowWidth / 2, 100);
-  let y = randomGaussian(windowHeight / 2, 100);
+  let x = randomGaussian(windowWidth / 2, slider.value() * 100);
+  let y = randomGaussian(windowHeight / 2, slider.value() * 100);
   let colorR = randomGaussian(150, 75);
   let colorG = randomGaussian(10, 5);
   let colorB = randomGaussian(100, 5);
