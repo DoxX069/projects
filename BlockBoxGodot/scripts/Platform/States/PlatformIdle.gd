3a99ce6747dfab0bc9f573cdca60fb00eafd0bbf
DoxX069 <ammonnils@gmail.com>
2025-05-22 17:09:07 +0200

    BlockBox remove
    Moved BlockBox project to own Repository

--- BlockBoxGodot/scripts/Platform/States/PlatformIdle.gd
+++
@@ -1,5 +0,0 @@
-extends PlatformState
-class_name PlatformIdle
-
-func physics_update(_delta: float) -> void:
-	pass
--- BlockBoxGodot/scripts/global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var intersection: Dictionary
-var last_intersection: Dictionary
-var dragged_block: Node3D
-var block_above
-var falling: bool = false
